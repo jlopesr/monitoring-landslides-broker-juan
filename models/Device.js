9659826a5a7bd47e13a4bc651fcf743de04df33b
LucasLeocardo <LucasLeocardo@users.noreply.github.com>
2022-05-17 19:30:05 -0300

    updating device model

--- models/Device.js
+++ models/Device.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 
 const Device = mongoose.model('Devices', {
     name: String,
+    latitude: Number,
+    longiude: Number,
+    isActive: { type: Boolean, default: true },
     created_at: { type: Date, default: Date.now }
 });
 
