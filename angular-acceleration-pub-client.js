322f262dee8248589dd535388448c0c3e192b45d
LucasLeocardo <LucasLeocardo@users.noreply.github.com>
2022-08-28 21:10:43 -0300

    Migrating to IotData collection

--- angular-acceleration-pub-client.js
+++ angular-acceleration-pub-client.js
@@ -2,7 +2,7 @@ var mqtt = require('mqtt');
 require('dotenv').config();
 //ws://localhost:1883
 //ws://monitoring-landslides-broker.herokuapp.com
-var topic = 'linearAccelerationData';
+var topic = 'angularAccelerationData';
 var deviceId = '62c8cf0794257003d4bf85e5';
 var username = process.env.BROKER_USER_NAME;
 var password = process.env.BROKER_PASSWORD;
@@ -13,9 +13,9 @@ client.on('connect', () => {
     setInterval(() => {
         var message = {
             deviceId: deviceId,
-            alphaX: getRandomInt(5, 15),
-            alphaY: getRandomInt(5, 15),
-            alphaZ: getRandomInt(5, 15)
+            alphaX: getRandomInt(-10, 10),
+            alphaY: getRandomInt(-10, 10),
+            alphaZ: getRandomInt(-10, 10)
         }
         client.publish(topic, JSON.stringify(message));
         console.log('===================message sent!===================');
--- linear-acceleration-pub-client.js
+++ linear-acceleration-pub-client.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 //ws://localhost:1883
 //ws://monitoring-landslides-broker.herokuapp.com
 var topic = 'linearAccelerationData';
-var deviceId = '62cdce9f55ac810106af0c35';
+var deviceId = '62c8cf0794257003d4bf85e5';
 var username = process.env.BROKER_USER_NAME;
 var password = process.env.BROKER_PASSWORD;
 var client = mqtt.connect('ws://monitoring-landslides-broker.herokuapp.com', {username, password});
@@ -13,9 +13,9 @@ client.on('connect', () => {
     setInterval(() => {
         var message = {
             deviceId: deviceId,
-            acelX: getRandomInt(5, 15),
-            acelY: getRandomInt(5, 15),
-            acelZ: getRandomInt(5, 15)
+            acelX: getRandomInt(-20, 20),
+            acelY: getRandomInt(-20, 20),
+            acelZ: getRandomInt(-20, 20)
         }
         client.publish(topic, JSON.stringify(message));
         console.log('===================message sent!===================');
